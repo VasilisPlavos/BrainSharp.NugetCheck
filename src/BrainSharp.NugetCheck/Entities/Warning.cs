d94c96089861a1e59c767a5819660c2276b1ef60
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-09-29 23:43:42 +0200

    solution

---
+++ src/BrainSharp.NugetCheck/Entities/Warning.cs
@@ -0,0 +1,10 @@
+﻿using BrainSharp.NugetCheck.Dtos;
+
+namespace BrainSharp.NugetCheck.Entities;
+
+public class Warning
+{
+    public string Message { get; set; }
+    public string BreadCrumb { get; set; }
+    public PackageMetadataRegistrationDto? Package { get; set; }
+}
